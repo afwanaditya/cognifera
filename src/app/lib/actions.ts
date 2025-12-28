ba14d02a727881fe13ce9d203ab11d7d53f28ddd
afwanaditya <afwanaditya2005@gmail.com>
2025-12-28 09:27:56 +0700

    bct

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,8 +1,8 @@
-import { revalidatePath } from "next/cache"; 
+// import { revalidatePath } from "next/cache"; 
 
 export async function deleteBookAction(bookId: number) {
   console.log(`--- DELETE ACTION TRIGGERED for ID: ${bookId} ---`);
 
 
-  revalidatePath("/dashboard/admin");
+  // revalidatePath("/dashboard/admin");
 }
